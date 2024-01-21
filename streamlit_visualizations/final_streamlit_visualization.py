18a0f0a3f9d8ab69f4b4479f1ab6b21606087a1b
Cristina Racovita <cristina.racovita98@gmail.com>
2024-01-21 22:47:14 +0100

    change path management library

--- streamlit_visualizations/final_streamlit_visualization.py
+++ streamlit_visualizations/final_streamlit_visualization.py
@@ -1,13 +1,16 @@
-import sys
-import path
-dir = path.Path(__file__).abspath()
-sys.path.append(dir.parent)
+# import sys
+# import path
+# dir = path.Path(__file__).abspath()
+# sys.path.append(dir.parent)
 import streamlit as st
 import pandas as pd
 from evaluation_methods import evaluate_approaches
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import plotly.express as px
+
+import pathlib
+code_dir = pathlib.Path(__file__).parent.resolve()
 
 def convert_bin_to_time_interval(bin_range):
     bin_start, bin_end = bin_range
