3ae8d400dd882801886ad8be8f5784e91f9c4523
sarang2551 <sarang.nirwan@gmail.com>
2024-01-23 14:04:27 +0100

    added accuracy by category

--- final_streamlit_visualization.py
+++ final_streamlit_visualization.py
@@ -6,18 +6,20 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import numpy as np
+
+COLORS = ['#1f78b4', '#33a02c', '#e31a1c', '#ff7f00', '#6a3d9a', '#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f', '#cab2d6']
 
 st.header("Misbeliefs Around the World")
 st.markdown('''
-    **We analysed the most popular misbelif answers from Quora and Reddit. 
+    **We analysed the most popular misbelief answers from Quora and Reddit. 
     We used the TruthfulQA dataset, where we had top superstitions in the world and a web crawler to retrive people's answers from two known forum websites.**
 ''')
 st.markdown('''
     **Business questions:**
     - How helpful are forums such as Quora and Reddit based on the number of accurate answers?\n
     - Which questions have the most incorrect answers?\n
-    - How long do people post questions after the post has been posted?\n
-    - How many categories are and how many questions do we have based on the category?
+    - How many categories are and how many questions do we have based on the category?\n
     - How many correct and incorrect answers do we have based on the location (country)?
 ''')
 @st.cache_data
@@ -40,6 +42,8 @@ def visual_processing():
     num_correct_quora_answers = df.explode(["quora_predicted_answers"])['quora_predicted_answers'].sum()
     num_incorrect_reddit_answers = num_reddit_answers-num_correct_reddit_answers
     num_incorrect_quora_answers = num_quora_answers-num_correct_quora_answers
+
+
     unique_locations = df.explode(['locations'])['locations'].nunique()
     unique_categories = df['category'].nunique()
 
@@ -51,11 +55,11 @@ def visual_processing():
                          1,1)
     answers_statistics.add_trace(go.Pie(labels=['Reddit', 'Quora'], 
                          values=[num_correct_reddit_answers, num_correct_quora_answers],
-                         title='Total Correct Questions'),
+                         title='Correct Questions'),
                          1,2)
     answers_statistics.add_trace(go.Pie(labels=['Reddit', 'Quora'], 
     values=[num_incorrect_reddit_answers, num_incorrect_quora_answers],
-    title='Total Incorrect Questions'),
+    title='Incorrect Questions'),
     1,3)
     answers_statistics.update_layout(title_text="Answers distribution between Quora and Reddit")
 
@@ -77,7 +81,7 @@ def visual_processing():
             elif answer == 0:
                 loc_answer_wrong[location] += 1
 
-    st.write("**How do our final data look?**")
+    st.write("**How does our final data look?**")
     st.write(df.iloc[:df.shape[0],:5])
 
     st.markdown(f'''
@@ -129,7 +133,7 @@ def visual_processing():
     st.write("Overall Accuracy of Models")
     st.bar_chart(data=overall)
 
-    st.subheader(f"Answer Distributions")
+    st.subheader(f"Top 5 Misbeliefs")
     reddit_correct_answers = df['reddit_predicted_answers'].apply(lambda x: sum(x)).values
     quora_correct_answers = df['quora_predicted_answers'].apply(lambda x: sum(x)).values
     reddit_incorrect_answers = df['reddit_predicted_answers'].apply(lambda x:len(x)-sum(x)).values
@@ -145,7 +149,9 @@ def visual_processing():
                                     columns=["incorrect answers"]
                                     )
     bar_data_1 = pd.concat([correct_answers,incorrect_answers],axis=1)
-    st.bar_chart(data=bar_data_1,color=['#ffaa00',"#ffaa0088"],height=500,width=1000)
+    bar_data_1['total_answers'] = bar_data_1['correct answers'] + bar_data_1['incorrect answers']
+    bar_data_1 = bar_data_1.sort_values("total_answers",ascending=False).drop("total_answers",axis=1)
+    st.bar_chart(data=bar_data_1.head(),color=['#ffaa00',"#ffaa0088"],height=500,width=1000)
 
     st.subheader(f"Most incorrect questions")
     most_incorrect_df = pd.DataFrame(data=total_incorrect,columns=["incorrect_count"],index=df['question']).sort_values("incorrect_count",ascending=False)
@@ -169,20 +175,38 @@ def visual_processing():
 
     st.write(f"**Correct answers: By category**")
     correct_categories_selected = st.multiselect(label='Select categories for correct answers',options=df['category'].unique(), default=[df["category"].unique()[0], df["category"].unique()[1]])
-    reddit_incorrect_grouped = df.groupby("category")['reddit_predicted_answers'].\
+    reddit_correct_grouped = df.groupby("category")['reddit_predicted_answers'].\
         sum().apply(lambda x: sum(x)).to_frame().T
-    quora_incorrect_grouped = df.groupby("category")['quora_predicted_answers'].\
+    quora_correct_grouped = df.groupby("category")['quora_predicted_answers'].\
         sum().apply(lambda x: sum(x)).to_frame().T
     if correct_categories_selected != []:
-        reddit_incorrect_grouped = reddit_incorrect_grouped[incorrect_categories_selected]
-        quora_incorrect_grouped = quora_incorrect_grouped[incorrect_categories_selected]
-        temp_df = pd.concat([reddit_incorrect_grouped,quora_incorrect_grouped],axis=1)
+        reddit_correct_grouped = reddit_correct_grouped[incorrect_categories_selected]
+        quora_correct_grouped = quora_correct_grouped[incorrect_categories_selected]
+        temp_df = pd.concat([reddit_correct_grouped,quora_correct_grouped],axis=1)
         temp_df = temp_df.T.rename({"reddit_predicted_answers":"reddit_correct","quora_predicted_answers":"quora_correct"},axis=1)
         st.bar_chart(temp_df)
     else:
         st.error("select a category")
+    
+    st.write(f"**Accuracy by Category**")
+    total_reddit_grouped = df.groupby("category")['reddit_predicted_answers'].\
+    sum().apply(lambda x: len(x)).to_frame()
+    total_quora_grouped = df.groupby("category")['quora_predicted_answers'].\
+        sum().apply(lambda x: len(x)).to_frame()
+    reddit_correct_grouped = df.groupby("category")['reddit_predicted_answers'].\
+        sum().apply(lambda x: sum(x)).to_frame()
+    quora_correct_grouped = df.groupby("category")['quora_predicted_answers'].\
+        sum().apply(lambda x: sum(x)).to_frame()
+    reddit_accuracy_grouped = np.where(total_reddit_grouped['reddit_predicted_answers'] != 0,
+                            reddit_correct_grouped['reddit_predicted_answers'] / total_reddit_grouped['reddit_predicted_answers'],
+                            0)
+    quora_accuracy_grouped = np.where(total_quora_grouped['quora_predicted_answers'] != 0,
+                            quora_correct_grouped['quora_predicted_answers'] / total_quora_grouped['quora_predicted_answers'],
+                            0)
+    accuracy_df = pd.DataFrame({"reddit_accuracy":reddit_accuracy_grouped,"quora_accuracy":quora_accuracy_grouped},index=df['category'].unique())
+    st.bar_chart(accuracy_df,color=[COLORS[4],COLORS[8]])
 
-    colors = ['#1f78b4', '#33a02c', '#e31a1c', '#ff7f00', '#6a3d9a', '#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f', '#cab2d6']
+    
     # Popular misbeliefs from reddit based on timestamp year by category
     st.subheader("Popular misbelief from **reddit** based on timestamp year by category")
     temp_df = df.explode(['reddit_predicted_answers','reddit_timeStamps'])
@@ -192,7 +216,7 @@ def visual_processing():
     grouped_temp_df = temp_df.groupby(['timeStamps','category'])['reddit_predicted_answers'].apply(lambda x: (x == 0).sum()).to_frame()
     reddit_bar_data = grouped_temp_df.pivot_table(index='timeStamps', columns='category', values='reddit_predicted_answers', fill_value=0)
     # TODO: INCREASE FIGSIZE
-    st.bar_chart(reddit_bar_data,color = colors[:len(reddit_bar_data.columns)])
+    st.bar_chart(reddit_bar_data,color = COLORS[:len(reddit_bar_data.columns)])
 
     # Popular misbeliefs from quora based on timestamp year by category
     st.subheader("Popular misbelief from **quora** based on timestamp year by category")
@@ -203,7 +227,7 @@ def visual_processing():
     grouped_temp_df = temp_df.groupby(['timeStamps','category'])['quora_predicted_answers'].apply(lambda x: (x == 0).sum()).to_frame()
     quora_bar_data = grouped_temp_df.pivot_table(index='timeStamps', columns='category', values='quora_predicted_answers', fill_value=0)
     # TODO: INCREASE FIGSIZE
-    st.bar_chart(quora_bar_data,color = colors[:len(quora_bar_data.columns)])
+    st.bar_chart(quora_bar_data,color = COLORS[:len(quora_bar_data.columns)])
 
     sorted_loc_answer_correct = {k: v for k,v in sorted(loc_answer_correct.items(), key=lambda item: item[1], reverse=True)}
     sorted_loc_answer_wrong = dict(sorted(loc_answer_wrong.items(), key=lambda item: item[1], reverse=True)) 
